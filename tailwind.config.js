335bf57a7dc452a0695c5afb845458f5525d91b7
Damilola Ale <codenlyn@gmail.com>
2024-08-23 13:16:32 +0100

    chore: update page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
